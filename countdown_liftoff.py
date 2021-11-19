dde833bd29bf9802c292091d47e7e7db55181e6f
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-11-19 14:09:56 -0700

    Add files via upload

---
+++ countdown_liftoff.py
@@ -0,0 +1,9 @@
+#This is a very simple countdown to liftoff for a space shuttle:
+countdown = 10
+print("Starting the countdown to liftoff!")
+while countdown >= 0:
+  #Print the current value of count.
+  print(countdown)
+  #Incremental counting
+  countdown -= 1
+print("We have liftoff!")
